8fe94e92a6027abb233cf1ab21b00a3429218821
Marc Catalan Sanchez <mcatalan@student.42barcelona.com>
2024-03-26 10:56:15 +0100

    adding philo

--- src/main.c
+++ src/main.c
@@ -6,37 +6,39 @@
 /*   By: mcatalan <mcatalan@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 11:19:05 by mcatalan@st       #+#    #+#             */
-/*   Updated: 2024/03/13 10:59:41 by mcatalan         ###   ########.fr       */
+/*   Updated: 2024/03/26 10:55:08 by mcatalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void	error_ext(char *str)
+void  error_ext(char *str)
 {
-	printf("%s\n", str);
-	exit (EXIT_FAILURE);
+  printf("%s\n", str);
+  exit(EXIT_FAILURE);
 }
 
-int	parsing(char **argv)
+int parsing(char **argv)
 {
-	for (int i = 0; argv[i]; i++)
-		printf("argv %d: %s\n", i, argv[i]);
-	return (0);
+  int i;
+
+  i = 0;
+  while (argv[i]) {
+    printf("argv %d: %s\n", i, argv[i]);
+    i++;
+  }
+  return (0);
 }
 
-int	exec_philo(char **argv)
-{
-	if (parsing(argv) == -1)
-		error_ext("Check arguments. Too long or not int");
-	return (0);
+int philo(char **argv) {
+  if (parsing(argv) == -1)
+    error_ext("Check arguments. Too long or not int");
+  return (0);
 }
 
-int	main(int argc, char **argv)
-{
-	if (argc == 5 || argc == 6)
-		exec_philo(argv);
-	else
-		error_ext("Wrong args. Philo needs 5 or 6 arguments");
-	return (0);
-}
+int main(int argc, char **argv) {
+  if (argc == 5 || argc == 6) {
+    philo(argv);
+    else error_ext("Wrong args. Philo needs 5 or 6 arguments");
+    return (0);
+  }
