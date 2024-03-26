08c79b20b12a624a81c5ab08bed0828dccad4a9f
mcatalan15 <marc.prat.2001@gmail.com>
2024-03-26 11:12:45 +0100

    updating

--- includes/philo.h
+++ includes/philo.h
@@ -3,73 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcatalan <mcatalan@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: mcatalan@student.42barcelona.com <mcata    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 13:03:49 by mcatalan          #+#    #+#             */
-/*   Updated: 2024/03/25 10:44:39 by mcatalan         ###   ########.fr       */
+/*   Updated: 2024/03/26 11:11:39 by mcatalan@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
 // System .h files
 // # include <string.h>
-#include <pthread.h> //mutex: init destroy lock unlock
-#include <stdbool.h> // boolean usage
-#include <stdio.h>   //printf
-#include <stdlib.h>  //malloc, free
-#include <sys/_pthread/_pthread_mutex_t.h>
-#include <sys/_pthread/_pthread_t.h>
-#include <unistd.h>   // write, usleep
+# include <pthread.h> //mutex: init destroy lock unlock
+# include <stdbool.h> // boolean usage
+# include <stdio.h>   //printf
+# include <stdlib.h>  //malloc, free
+# include <sys/_pthread/_pthread_mutex_t.h>
+# include <sys/_pthread/_pthread_t.h>
+# include <unistd.h>   // write, usleep
                       //threads: create join detach
-#include <limits.h>   // INT_MAX
-#include <sys/time.h> //gettimeofday
+# include <limits.h>   // INT_MAX
+# include <sys/time.h> //gettimeofday
 
 // colors
-#define RED "\033[0;31m"
-#define GREEN "\033[0;32m"
-#define YELLOW "\033[0;33m"
-#define BLUE "\033[0;34m"
-#define MAGENTA "\033[0;35m"
-#define CYAN "\033[0;36m"
-#define RESET "\033[0m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define MAGENTA "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define RESET "\033[0m"
 
 // structs declaration
-typedef pthread_mutex_t t_mtx;
-typedef struct s_fork t_fork;
-typedef struct s_philo t_philo;
-typedef struct s_table t_table;
+typedef pthread_mutex_t	t_mtx;
+typedef struct s_fork	t_fork;
+typedef struct s_philo	t_philo;
+typedef struct s_table	t_table;
 
-typedef struct s_fork {
-  t_mtx fork;
-  int id_fork;
-} t_fork;
+typedef struct s_fork
+{
+	t_mtx	fork;
+	int		id_fork;
+}			t_fork;
 
-typedef struct s_philo {
-  int id_philo;
-  int c_meal; // counter of meals
-  int full;   // bool?
-  int l_meal; // last meal time
-  t_fork *l_fork;
-  t_fork *r_fork;
-  pthread_t id_thread;
-} t_philo;
+typedef struct s_philo
+{
+	int			id_philo;
+	int			c_meal; // counter of meals
+	int			full;   // bool?
+	int			l_meal; // last meal time
+	t_fork		*l_fork;
+	t_fork		*r_fork;
+	pthread_t	id_thread;
+}				t_philo;
 
-typedef struct s_table {
-  int n_philo;
-  int t_die;   // time to die
-  int t_eat;   // time to eat
-  int t_sleep; // time to sleep
-  int m_meals; // max meals (argv[5])
-  int s_time;  // time_start
-  int end;
-  t_fork *forks;
-  t_philo *philo;
-} t_table;
+typedef struct s_table
+{
+	int		n_philo;
+	int		t_die;   // time to die
+	int		t_eat;   // time to eat
+	int		t_sleep; // time to sleep
+	int		m_meals; // max meals (argv[5])
+	int		s_time;  // time_start
+	int		end;
+	t_fork	*forks;
+	t_philo	*philo;
+}			t_table;
 
 // utils
 // utils.c
-int ft_atoi(const char *str);
+int	ft_atoi(const char *str);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,42 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcatalan <mcatalan@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: mcatalan@student.42barcelona.com <mcata    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 11:19:05 by mcatalan@st       #+#    #+#             */
-/*   Updated: 2024/03/26 10:55:08 by mcatalan         ###   ########.fr       */
+/*   Updated: 2024/03/26 11:04:00 by mcatalan@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void  error_ext(char *str)
+void	error_ext(char *str)
 {
-  printf("%s\n", str);
-  exit(EXIT_FAILURE);
+	printf("%s\n", str);
+	exit(EXIT_FAILURE);
 }
 
-int parsing(char **argv)
+int	parsing(char **argv)
 {
-  int i;
+	int	i;
 
-  i = 0;
-  while (argv[i]) {
-    printf("argv %d: %s\n", i, argv[i]);
-    i++;
-  }
-  return (0);
+	i = 0;
+	while (argv[i])
+	{
+		printf("argv %d: %s\n", i, argv[i]);
+		i++;
+	}
+	return (0);
 }
 
-int philo(char **argv) {
-  if (parsing(argv) == -1)
-    error_ext("Check arguments. Too long or not int");
-  return (0);
+int	philo(char **argv)
+{
+	if (parsing(argv) == -1)
+		error_ext("Check arguments. Too long or not int");
+	return (0);
 }
 
-int main(int argc, char **argv) {
-  if (argc == 5 || argc == 6) {
-    philo(argv);
-    else error_ext("Wrong args. Philo needs 5 or 6 arguments");
-    return (0);
-  }
+int	main(int argc, char **argv)
+{
+	if (argc == 5 || argc == 6)
+		philo(argv);
+	else
+		error_ext("Wrong args. Philo needs 5 or 6 arguments");
+	return (0);
+}
