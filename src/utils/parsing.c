188ad6d8576b953ec519b6d098349b70f98a2de7
Marc Catalan Sanchez <mcatalan@student.42barcelona.com>
2024-05-01 10:49:00 +0200

    adding ft_isdigit

--- src/utils/parsing.c
+++ src/utils/parsing.c
@@ -6,11 +6,26 @@
 /*   By: mcatalan <mcatalan@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 11:55:09 by mcatalan          #+#    #+#             */
-/*   Updated: 2024/04/29 10:47:26 by mcatalan         ###   ########.fr       */
+/*   Updated: 2024/05/01 10:48:46 by mcatalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philo.h"
+
+int	ft_is_digit(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 0 && str[i] <= 9)
+			i++;
+		else
+			return (EXIT_FAILURE);
+	}
+	return (0);
+}
 
 /*
 	This is the second part of the main parsing function. Just checks the
@@ -38,12 +53,17 @@ int	parsing(char **argv)
 	i = 0;
 	while (argv[++i])
 	{
-		if ((ft_atol(argv[i]) < 1 || ft_atol(argv[i]) > 200) && i == 1)
-			return (print_err(ERROR_NPHILO));
-		else if ((ft_atol(argv[i]) < 60 || ft_atol(argv[i]) > INT_MAX)
-			&& (i > 1 && i < 5))
-			return (print_err(ERROR_NTIME));
-		else if (parsing2(argv, i))
+		if (ft_is_digit(argv[i]) == 0)
+		{
+			if ((ft_atol(argv[i]) < 1 || ft_atol(argv[i]) > 200) && i == 1)
+				return (print_err(ERROR_NPHILO));
+			else if ((ft_atol(argv[i]) < 60 || ft_atol(argv[i]) > INT_MAX)
+				&& (i > 1 && i < 5))
+				return (print_err(ERROR_NTIME));
+			else if (parsing2(argv, i))
+				return (EXIT_FAILURE);
+		}
+		else
 			return (EXIT_FAILURE);
 	}
 	return (0);
